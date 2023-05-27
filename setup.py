8d57cb1b3e74786a240e8e400197f6a00a44312c
Aziyan99 <rajaazian08@gmail.com>
2023-05-27 12:13:16 +0700

    rename

--- setup.py
+++ setup.py
@@ -9,15 +9,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 extensions = [
-    setuptools.Extension('sword2vec_INIEZZY.helpers', [os.path.join(basedir, 'src/sword2vec_INIEZZY/utils/helpers.pyx')])
+    setuptools.Extension(
+        "sword2vec.helpers",
+        [os.path.join(basedir, "src/sword2vec/utils/helpers.pyx")],
+    )
 ]
 
 setuptools.setup(
-    name="sword2vec_INIEZZY",
+    name="sword2vec",
     version="0.0.1",
     author="Raja Azian",
     author_email="rajaazian08@gmail.com",
-    description="A simple skipgram word2vec implementation package",
+    description="A simple skipgram word2vec implementations",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/aziyan99/sword2vec",
@@ -26,17 +29,17 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.11",
     packages=setuptools.find_packages(),
     ext_modules=cythonize(extensions),
     install_require=[
-        'click==8.1.3',
-        'colorama==0.4.6',
-        'Cython==0.29.35',
-        'joblib==1.2.0',
-        'nltk==3.8.1',
-        'numpy==1.24.3',
-        'regex==2023.5.5'
+        "click==8.1.3",
+        "colorama==0.4.6",
+        "Cython==0.29.35",
+        "joblib==1.2.0",
+        "nltk==3.8.1",
+        "numpy==1.24.3",
+        "regex==2023.5.5",
     ],
-    include_dirs=[numpy.get_include()]
+    include_dirs=[numpy.get_include()],
 )
