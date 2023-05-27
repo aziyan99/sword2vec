031927c1f369216780a808fdb59efe50228b28f7
Aziyan99 <rajaazian08@gmail.com>
2023-05-27 13:15:44 +0700

    add run for test

---
+++ run.py
@@ -0,0 +1,28 @@
+from sword2vec import SkipGramWord2Vec
+
+model = SkipGramWord2Vec(window_size=2, learning_rate=0.025, embedding_dim=2, epochs=5)
+
+lines = []
+with open("./datasets_judul_buku_full.txt", encoding="utf8") as f:
+    for line in f.readlines():
+        lines.append(line)
+    f.close()
+
+model.train(lines=lines)
+model.save_compressed_model("model")
+
+loaded_model = SkipGramWord2Vec.load_compressed_model("./model")
+
+word_test = "belajar"
+
+# use general predict
+pred_words = loaded_model.predict(word=word_test, topn=20)
+
+print("\npredict:\n")
+for word in pred_words:
+    print(word)
+
+print("\ncosine similarity:\n")
+cos_words = loaded_model.search_similar_words(word=word_test, topn=20)
+for word in cos_words:
+    print(word)
